1adbf1cff31b27ea883f8c809ed4a44cb9f7e886
Gurjaipal Singh <163448707+Gurjaipal17@users.noreply.github.com>
2024-09-04 22:05:24 +0530

    Update ApiRoutes.js

--- client/src/utils/ApiRoutes.js
+++ client/src/utils/ApiRoutes.js
@@ -1,6 +1,6 @@
 
 
-export const HOST = "http://localhost:3005";
+export const HOST = "https://chat-wave-server-delta.vercel.app/";
 
 const authRoute = `${HOST}/api/auth`;
 const MESSAGES_ROUTE = `${HOST}/api/messages`;
